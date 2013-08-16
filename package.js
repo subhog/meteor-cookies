c3d2684cd97dd611652833169b5f628ad23c57f6
Hubert OG <hubert@orlikarnia.com>
2013-08-16 15:20:59 +0200

    Update for Meteor 0.6.5

--- package.js
+++ package.js
@@ -3,5 +3,9 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
+  if(api.export) {
+    api.use('deps', 'client');
+    api.export('Cookie', 'client');
+  }
   api.add_files('cookie.js', 'client');
 });
